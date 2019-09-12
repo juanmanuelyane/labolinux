7cdc36db6c28f00b55ee41b76ee0061df751f62a
Juan <juanmanuelyane@gmail.com>
2019-09-12 15:27:10 -0300

    asdasd

---
+++ minarr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nuevohash="UNSAM administracion GNU/linux"
+cadena="UNSAM administracion GNU/linux"
+
+while [  "${nuevohash:0:1}" != "0" ]
+do
+    cadena="$cadena$RANDOM" #agregar caracteres a la cadena
+    nuevohash=`echo $cadena | sha1sum`
+    echo $cadena
+    echo $nuevohash
+done
