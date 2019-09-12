9d2b8b1dfe5d097ec0f97b0df172bb8e29023671
Juan <juanmanuelyane@gmail.com>
2019-09-12 15:42:09 -0300

    qqq

--- minarr.sh
+++ minarr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nuevohash="UNSAM administracion GNU/linux"
 cadena="UNSAM administracion GNU/linux"
-
+echo "qqq"
 while [  "${nuevohash:0:1}" != "0" ]
 do
     cadena="$cadena$RANDOM" #agregar caracteres a la cadena
